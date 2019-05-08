ed2eba740a902c78720b59e7412ae8288cfaa029
Luis Plazas <lf.plazas10@uniandes.edu.co>
2019-05-07 23:24:18 -0500

    Better error management

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -2,11 +2,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
-
 public class EntryPoint {
 
     final static Logger logger = LoggerFactory.getLogger(EntryPoint.class);
@@ -14,28 +9,28 @@ public class EntryPoint {
     //Initialize env vars with .env
     public final static Dotenv dotenv = Dotenv.load();
 
-    public static void main(String[] args) throws Exception{
-        //Validate docker is running
-        DockerManager dockerManager = new DockerManager();
-
-        //Validate docker registry is reachable
-        dockerManager.testPrivateRegistryConnection();
-        logger.info("Registry contacted successfully");
-
-        //Create connection to the queue
-
-
+    public static void main(String[] args) {
         try {
+            //Validate docker is running
+            DockerManager dockerManager = new DockerManager();
+
+            //Validate docker registry is reachable
+            dockerManager.testPrivateRegistryConnection();
+            logger.info("Registry contacted successfully");
+
+            //Create connection to the queue
+
+
             while (true) {
-                try{
+                try {
 //                    Socket s=serverSocket.accept();
 //                    ExecutorService.executeRequestTask(new ClouderServerAttentionThread(s));
-                }catch (Exception ex) {
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            logger.error("Error during start up", ex);
         }
 
 
