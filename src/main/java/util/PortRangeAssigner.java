62550f0eb84518af5ca6c3d078c219c10b2e7cf3
Luis Plazas <lf.plazas10@uniandes.edu.co>
2019-05-13 10:26:14 -0500

    Generating free range of ports from util functions created

--- src/main/java/util/PortRangeAssigner.java
+++ src/main/java/util/PortRangeAssigner.java
@@ -1,4 +1,56 @@
 package util;
 
+import javax.net.ServerSocketFactory;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class PortRangeAssigner {
+
+    private static final Integer START_PORT = 30000;
+    private static final Integer AMOUNT_RANGE = Conf.AMOUNT_EXPOSED_PORTS;
+
+    /**
+     *
+     * @param assignedRanges, is not null
+     */
+    public static Integer getPortRange(List<Integer> assignedRanges){
+        assignedRanges.add(START_PORT);
+        Collections.sort(assignedRanges);
+        Integer startRange = assignedRanges.get(assignedRanges.size()-1) + AMOUNT_RANGE;
+        while (!isPortRangeAvailable(startRange, startRange + AMOUNT_RANGE)){
+            startRange += AMOUNT_RANGE;
+        }
+        System.out.println(startRange);
+        return startRange;
+    }
+
+    /**
+     *
+     * @param start inclusive
+     * @param end not inclusive
+     * @return
+     */
+    private static boolean isPortRangeAvailable(int start, int end){
+        boolean isAvailable = true;
+        for (int i = start; i < end && isAvailable; i++) {
+            isAvailable = isPortAvailable(i);
+        }
+        return isAvailable;
+    }
+
+    private static boolean isPortAvailable(int port) {
+        try {
+            ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(
+                    port, 1, InetAddress.getByName("localhost"));
+            serverSocket.close();
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
 }
